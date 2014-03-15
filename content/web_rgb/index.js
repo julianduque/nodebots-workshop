63aec5023c3c64d2d55e4b79fa0aa3bfc719046d
Julian Duque <julianduquej@gmail.com>
2014-03-15 06:35:08 -0300

    Add Web RGB project

--- content/web_rgb/index.js
+++ content/web_rgb/index.js
@@ -1,13 +1,45 @@
-var http = require('http'),
-    express = require('express')
+var five    = require('johnny-five'),
+    http    = require('http'),
+    express = require('express'),
+    app     = express(),
+    server  = http.createServer(app),
+    board   = new five.Board(),
+    rgb;
 
-var app = express();
-var server = http.createServer(app);
+//
+// Start RGB Led
+//
+board.on('ready', function () {
+  rgb = new five.Led.RGB([3, 5, 6]);
+});
 
-app.get('/on', function (req, res) {});
-app.get('/off', function (req, res) {});
-app.get('/color', function (req, res) {
-  console.log(req.param);
+//
+// HTTP Server
+//
+app.use(function (req, res, next) {
+  if (rgb) return next();
+
+  res.writeHead(503, { 'Content-Type': 'text/plain' });
+  res.end('Arduino not ready');
+});
+
+app.get('/', function (req, res) {
+  var color = req.query.color || '#FF0000';
+  rgb.color(color);
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
+  res.end('Led is ' + color);
+});
+
+app.get('/on', function (req, res) {
+  rgb.on();
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
+  res.end('Led is on');
+});
+
+app.get('/off', function (req, res) {
+  rgb.off();
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
+  res.end('Led is off');
 });
 
 server.listen(8080);
